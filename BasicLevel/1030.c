aa82d02de4abd32194b0254d725cc31038ecd3b5
pzz <448116718@qq.com>
2019-08-03 16:21:19 +0800

    complete 1030

--- BasicLevel/1030.c
+++ BasicLevel/1030.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int cmp(const void *a, const void *b) {
+    return *(int *) a - *(int *) b;
+}
+
 int main() {
     int N, p, m = 0, M = 0, count = 0;
     scanf("%d %d", &N, &p);
@@ -8,12 +12,15 @@ int main() {
     for (int i = 0; i < N; i++)
         scanf("%d", &nums[i]);
 
-    for (int i = 0; i < N; i++) {
-        if (nums[i] < m)
-            m = nums[i];
-        if (nums[i] > M)
-            M = nums[i];
+    qsort(nums, N, sizeof(int), cmp);
+    int first, last, max = 0;
+    for (first = 0, last = 0; last < N && max < N - first; first++) {
+        while (last < N && nums[last] <= 1L * p * nums[first])
+            last++;
+        if (max < last - first)
+            max = last - first;
     }
+    printf("%d", max);
 
     return 0;
 }
